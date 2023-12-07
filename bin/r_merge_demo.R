c8250ad010b5407fc9904e36ee60dc99045ebda2
Josh Lamstein <jdlamstein@gmail.com>
2023-12-07 10:43:00 -0800

    r demo merging sql

--- bin/r_merge_demo.R
+++ bin/r_merge_demo.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-folder = "/gladstone/finkbeiner/linsley/Kaushik/GXYTMPS/GXYTMP-20231109-1-MsN-cry2tdp43-updated/CSVS"
+folder = "/gladstone/finkbeiner/linsley/Kaushik/GXYTMPS/GXYTMP-20231109-4-MsN-optocrispr/CSVS"
 
 
 experimentdata = read.csv(file.path(folder, "experimentdata.csv"))
@@ -10,23 +10,38 @@ celldata = read.csv(file.path(folder, "celldata.csv"))
 channeldata = read.csv(file.path(folder, "channeldata.csv"))
 dosagedata = read.csv(file.path(folder, "dosagedata.csv"))
 intensitycelldata = read.csv(file.path(folder, "intensitycelldata.csv"))
+# Rename id column to channeldata_id. The id may get replaced by another dataframe's id column. This avoids confusion. However, if you do not wish to rename columns, you can
+# merge with df <- merge(df1, df2, by.x='column from df1', by.y'='column from df2')
+colnames(channeldata)[colnames(channeldata) == "id"] = "channeldata_id"
+colnames(welldata)[colnames(welldata) == "id"] = "welldata_id"
+colnames(tiledata)[colnames(tiledata) == "id"] = "tiledata_id"
+colnames(celldata)[colnames(celldata) == "id"] = "celldata_id"
 colnames(channeldata)[colnames(channeldata) == "id"] = "channeldata_id"
 
 channel_names <- c('GFP-DMD1', 'RFP1', 'Cy5', 'Brightfield', 'BLUE-DMD-blocked', 'DAPI-DMD')
 channeldata <- filter(channeldata, channel %in% channel_names)
 glimpse(channeldata)
 
+# Check if you have the intensity per cell for your channel. If the channels are not present after the merge, then run the Intensity for your channel.
 intensitycelldata_channeldata <- merge(intensitycelldata, channeldata, by='channeldata_id', suffixes=c("", ".dontuse"))
 print(unique(intensitycelldata_channeldata$channel))
-df1 <- merge(tiledata, welldata, by.x='welldata_id', by.y='id',suffixes=c("", ".dontuse"))
 
-
-
-df2 <- merge(celldata, df1, by.x='tiledata_id', by.y='id', suffixes=c("", ".dontuse"))
+df1 <- merge(tiledata, welldata, by='welldata_id',suffixes=c("", ".dontuse"))
+print(unique(df1$well))  # List wells
+print(unique(df1$timepoint))  # List timepoints
+print(unique(df1$condition))  # List conditions (control, disease, stim, unstim,)
+df2 <- merge(df1, dosagedata, by='welldata_id', suffixes=c("", ".dontuse"))
 glimpse(df2)
-df3 <- merge(intensitycelldata, df2, by.x='celldata_id', by.y='id', suffixes=c("", ".dontuse"))
+print(unique(df2$name))  # Name of the drug or treatment or biosensor
+print(unique(df2$kind))  # Show the kind of drug or treatment or biosensor
+df3 <- merge(celldata, df2, by='tiledata_id', suffixes=c("", ".dontuse"))
 glimpse(df3)
-df4 <- merge(df3, channeldata, by.x='channeldata_id', by.y='id', suffixes=c("", ".dontuse"))
-data <- df4 %>% select(-contains("dontuse"))
+print(unique(df3$stimulate))  # See if dataset has DMD Stimulate, Values: False and True
+df4 <- merge(intensitycelldata, df3, by='celldata_id', suffixes=c("", ".dontuse"))
+glimpse(df4)
+df5 <- merge(df4, channeldata, by='channeldata_id', suffixes=c("", ".dontuse"))
+data <- df5 %>% select(-contains("dontuse"))
+print(unique(df4$channel))
+
 
 
